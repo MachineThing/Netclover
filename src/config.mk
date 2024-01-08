ebae76cec9925b64c04be1ce0efbbceebfa6649a
Mason Fisher <sackzombie10@gmail.com>
2024-01-08 12:30:58 -0500

    Fix .gitignore makefile bug and making libs folder

--- src/config.mk
+++ src/config.mk
@@ -1,13 +1,16 @@
-QEMU=		qemu-system-x86_64
+QEMU:=		qemu-system-x86_64
 
-TARGET=		i686-elf
-CC=			$(TARGET)-gcc
-CFLAGS=		-ffreestanding -nostdlib -lgcc
-ASM=		nasm
+TARGET:=	i686-elf
+CC:=		$(TARGET)-gcc
+CFLAGS:=	-ffreestanding -nostdlib -lgcc
+ASM:=		nasm
 #ASMFLAGS=	-f elf
 
-LD=			$(TARGET)-gcc
-LDFLAGS=	-nostdlib
+LD:=		$(TARGET)-gcc
+LDFLAGS:=	-nostdlib
 
-GITIGNORE=	$(shell cat .gitignore | grep -v '^#' | grep -v '^$$')
-DIRFILES=	$(shell find $1 -type f $(foreach pattern,$(GITIGNORE),-not -path '$(pattern)'))
+CPWD:=		$(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
+PWD:=		$(abspath $(CPWD)/..)
+
+GITIGNORE:=	$(shell cat $(PWD)/.gitignore | grep -v '^#' | grep -v '^$$')
+DIRFILES:=	$(shell find $1 -type f $(foreach pattern,$(GITIGNORE),-not -path '$(pattern)'))
