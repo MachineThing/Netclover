04a67bde99c26b7623ee716f4c8c9954c4893e8f
Mason Fisher <sackzombie10@gmail.com>
2024-03-04 06:33:12 -0500

    In compatibility mode!

--- src/kernel/prekernel.asm
+++ src/kernel/prekernel.asm
@@ -6,7 +6,7 @@ extern main
 section .kinit
 kinit:
     ; Setup basic paging
-    mov eax, (init_page_dir_tab - 0xC0000000)   ; Location in phys memory
+    mov eax, (init_page_pml4 - 0xC0000000)   ; Location in phys memory
     mov cr3, eax
 
     ; Enable PAE
@@ -14,16 +14,25 @@ kinit:
     or edx, (1 << 5)
     mov cr4, edx
 
+    ; Set LM Bit
+    mov ecx, 0xC0000080          ; 0xC0000080 is the EFER MSR.
+    rdmsr
+    or eax, 1 << 8               ; Set the LM-bit which is the 9th bit (bit 8).
+    wrmsr
+
     ; Enable paging
     mov ebx, cr0
     or ebx, (1 << 31)
     mov cr0, ebx
-    xchg bx, bx
 
     jmp main
 
 section .data
 align 32
+init_page_pml4:
+dq ((init_page_dir_tab-$$-0xC0000000) | 1) + $$     ; 0x0
+times 512-1 dq 0
+
 init_page_dir_tab:
 ; Note: We subtract $$ from the directory pointer
 ; and add $$ to it later so it can be logical or'ed
@@ -32,6 +41,7 @@ dq ((init_page_dir-$$-0xC0000000) | 1) + $$     ; 0x0
 dq 0
 dq 0
 dq ((init_page_dir-$$-0xC0000000) | 1) + $$     ; 0xC0000000
+times 512-4 dq 0
 
 align 4096
 init_page_dir:
